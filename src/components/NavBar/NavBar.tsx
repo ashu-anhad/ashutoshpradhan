8c1cf8dace594da14502bc660d5526cf368e6127
Ashutosh Pradhan <ashuanhad@gmail.com>
2022-11-18 12:05:20 +0530

    Top bar and Nav bar components added, bootstrap added.

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './NavBar.scss';
+
+const NavBar = (): any => {
+    return (
+        <div className="nav-bar">Nav Bar</div>
+    )
+}
+
+export default NavBar
---
+++ src/components/TopBar/TopBar.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './TopBar.scss';
+
+const TopBar = (): any => {
+    return (
+        <div className="top-bar">Top Bar</div>
+    )
+}
+
+export default TopBar
