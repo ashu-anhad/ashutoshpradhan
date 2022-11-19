ad6fe4331f50d34b0eab4a9367e1b42a9ce87efb
Ashutosh Pradhan <ashuanhad@gmail.com>
2022-11-19 10:59:17 +0530

    Topbar updated.

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -1,10 +1,25 @@
 import React from "react";
-import './TopBar.scss';
+import "./TopBar.scss";
 
 const TopBar = (): any => {
-    return (
-        <div className="top-bar">Top Bar</div>
-    )
-}
+  return (
+    <div className="top-bar clearfix">
+      <a href="#section8" className="available">
+        <i className="fa-regular fa-envelope"></i>
+        <span>Available For Freelance</span>
+      </a>
+      <div className="right-icons">
+        <a
+          className="download"
+          href="https://drive.google.com/file/d/12Jo-licvAXe-T9b8ws3iMCv4dR070GDa/view?usp=sharing"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fa-solid fa-cloud-arrow-down"></i>
+        </a>
+      </div>
+    </div>
+  );
+};
 
-export default TopBar
+export default TopBar;
