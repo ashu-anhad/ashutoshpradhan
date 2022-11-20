61271e1a043bd645e92cc7e096fd7f8465e885d5
Ashutosh Pradhan <ashuanhad@gmail.com>
2022-11-20 16:05:39 +0530

    Skills tab updated.

---
+++ src/components/Skills/ChangingProgressProvider.tsx
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+
+const ChangingProgressProvider = (props: any) => {
+  var [perValue, setPerValue] = useState(0);
+
+  useEffect(() => {
+    const delay = (i: any, index: any) => {
+      setTimeout(() => {
+        setPerValue(i);
+      }, props.interval * index);
+    };
+    props.values.map((per: any, index: any) => delay(per, index));
+  }, [props.values]);
+
+  return props.children(perValue);
+};
+
+export default ChangingProgressProvider;
