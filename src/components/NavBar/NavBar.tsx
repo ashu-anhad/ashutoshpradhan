5d75a711ae8d5dc556b8c27efe3710e39c8d6c2f
Ashutosh Pradhan <ashuanhad@gmail.com>
2022-11-20 20:54:50 +0530

    Responsive menubar added.

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,88 +1,116 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import "./NavBar.scss";
 
 const NavBar = (): any => {
-  return (
-    <div className="nav-bar">
-      <div className="bottom clearfix">
-        <div className="title">
-          <Link to="/">Ashutosh</Link>
-        </div>
+  const [isMenuVisible, setMenuVisibility] = useState(false);
 
-        <Link className="responsive-menu-open" to="/about">
-          Menu <i className="fa fa-bars"></i>
-        </Link>
-        <nav className="main-nav">
-          <ul className="list-unstyled">
-            <li>
-              <NavLink
-                className={(isActive) => (isActive.isActive ? "active" : "")}
-                to="/"
-              >
-                Home
-              </NavLink>
-            </li>
-            <li className="">
-              <NavLink
-                className={(isActive) => (isActive.isActive ? "active" : "")}
-                to="/about"
-              >
-                About
-              </NavLink>
-            </li>
-            <li className="">
-              <NavLink
-                className={(isActive) => (isActive.isActive ? "active" : "")}
-                to="/skill"
-              >
-                Skill
-              </NavLink>
-            </li>
-            <li className="">
-              <NavLink
-                className={(isActive) => (isActive.isActive ? "active" : "")}
-                to="/experience"
-              >
-                Experience
-              </NavLink>
-            </li>
-            <li className="">
-              <NavLink
-                className={(isActive) => (isActive.isActive ? "active" : "")}
-                to="/education"
-              >
-                Education
-              </NavLink>
-            </li>
-            <li className="">
-              <NavLink
-                className={(isActive) => (isActive.isActive ? "active" : "")}
-                to="/work"
-              >
-                Work
-              </NavLink>
-            </li>
-            <li className="">
-              <NavLink
-                className={(isActive) => (isActive.isActive ? "active" : "")}
-                to="/blog"
-              >
-                Blog
-              </NavLink>
-            </li>
-            <li className="">
-              <NavLink
-                className={(isActive) => (isActive.isActive ? "active" : "")}
-                to="/contact"
-              >
-                Contact
-              </NavLink>
-            </li>
-          </ul>
+  const toggleResponsiveMenu = () => {
+    setMenuVisibility(!isMenuVisible);
+  };
+
+  const getMenuOptions = () => {
+    return (
+      <ul className="list-unstyled">
+        <li>
+          <NavLink
+            className={(isActive) => (isActive.isActive ? "active" : "")}
+            to="/"
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={(isActive) => (isActive.isActive ? "active" : "")}
+            to="/about"
+          >
+            About
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={(isActive) => (isActive.isActive ? "active" : "")}
+            to="/skill"
+          >
+            Skill
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={(isActive) => (isActive.isActive ? "active" : "")}
+            to="/experience"
+          >
+            Experience
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={(isActive) => (isActive.isActive ? "active" : "")}
+            to="/education"
+          >
+            Education
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={(isActive) => (isActive.isActive ? "active" : "")}
+            to="/work"
+          >
+            Work
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={(isActive) => (isActive.isActive ? "active" : "")}
+            to="/blog"
+          >
+            Blog
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={(isActive) => (isActive.isActive ? "active" : "")}
+            to="/contact"
+          >
+            Contact
+          </NavLink>
+        </li>
+      </ul>
+    );
+  };
+
+  return (
+    <>
+      <div className="nav-bar">
+        <div className="bottom clearfix">
+          <div className="title">
+            <Link to="/">Ashutosh</Link>
+          </div>
+          <a
+            rel="noopener noreferrer"
+            className="responsive-menu-open"
+            onClick={toggleResponsiveMenu}
+          >
+            Menu <i className="fa fa-bars"></i>
+          </a>
+          <nav className="main-nav">{getMenuOptions()}</nav>
+        </div>
+      </div>
+
+      <div className={`responsive-menu ${isMenuVisible ? "open" : "close"}`}>
+        <a
+          rel="noopener noreferrer"
+          className="responsive-menu-close"
+          onClick={toggleResponsiveMenu}
+        >
+          Close <i className="fa-solid fa-xmark"></i>
+        </a>
+        <nav className="responsive-nav" onClick={toggleResponsiveMenu}>
+          {getMenuOptions()}
         </nav>
       </div>
-    </div>
+    </>
   );
 };
 
